33324fc743768742d67f4a36b2b92d702227d16e
souravmansingh <souravmansingh@192.168.0.102>
2020-03-30 09:32:55 +0530

    Initial Commit

---
+++ spring-payment-paytm/src/test/java/com/eomaxl/spring/paytm/api/SpringPaymentPaytmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eomaxl.spring.paytm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentPaytmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
